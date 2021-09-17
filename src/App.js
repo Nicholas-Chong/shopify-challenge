e3d2637bd0d95e317521e243284b1911135c7808
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-09-16 23:46:37 -0400

    Implement loading state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Heading, ChakraProvider, SimpleGrid, theme } from '@chakra-ui/react'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { AppContainer } from './components/Background'
 import { ImageCard } from './components/ImageCard'
-import { demo } from './demo'
+import { LoadingSpinner } from './components/LoadingSpinner'
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(false)
+  const [imagesData, setImagesData] = useState([])
+
+  useEffect(async () => {
+    let tmp = null
+
+    let startDate = new Date()
+    startDate.setMonth(startDate.getMonth() - 3)
+    startDate = startDate.toLocaleDateString('fr-CA')
+
+    await fetch(
+      `https://api.nasa.gov/planetary/apod?start_date=${startDate}&api_key=fusMDa3hRjdVjwuaweS6gsIjVDcmN7cmjsaj7nnX`
+    )
+      .then((response) => response.json())
+      .then((data) => (tmp = data))
+
+    setImagesData(tmp)
+    setImagesLoaded(true)
+  })
+
   return (
     <ChakraProvider theme={theme}>
       <AppContainer>
         <Heading mb={4}>ShopiSpace</Heading>
-        <SimpleGrid columns={[1, 2, 3, 4]} spacing={10}>
-          {demo.map(element => {
-            if (element.media_type === "image")
-              return <ImageCard image={element} />
-            else
-              return null
-            })
-          }
-        </SimpleGrid>
+        {!imagesLoaded && <LoadingSpinner/> }
+        {imagesLoaded && 
+          <SimpleGrid columns={[1, 2, 3, 4]} spacing={10}>
+            {imagesData.map((element) => {
+              if (element.media_type === 'image')
+                return <ImageCard image={element} />
+              else return null
+            })}
+          </SimpleGrid>
+        }
       </AppContainer>
     </ChakraProvider>
   )
