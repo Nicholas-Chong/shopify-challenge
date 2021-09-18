39f3036e49470f8e6333fe92e46dacad1d0c1937
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-09-18 11:31:09 -0400

    Set filled/outline heart based on if image is in likedImages indexdDb

--- src/components/ImageCard/LikeButton/LikeButton.js
+++ src/components/ImageCard/LikeButton/LikeButton.js
@@ -1,22 +1,30 @@
 import { Box } from '@chakra-ui/layout'
 import { IconContext } from 'react-icons'
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ImageContext } from '../ImageCard'
 import { AppContext } from '../../Background'
-import { addLikedImage, deleteLikedImage } from '../../../utilities/index-db'
+import {
+  addLikedImage,
+  deleteLikedImage,
+  searchForImage,
+} from '../../../utilities/index-db'
 
 export const LikeButton = () => {
   const [isLiked, setIsLiked] = useState(false)
   const [image] = useState(useContext(ImageContext))
   const [indexDb] = useState(useContext(AppContext))
 
+  useEffect(() => {
+    searchForImage(indexDb, image.title).then((result) => {
+      if (result) setIsLiked(true)
+    })
+  })
+
   const handleClick = () => {
-    if (isLiked)
-      deleteLikedImage(indexDb, image)
-    else
-      addLikedImage(indexDb, image)
-      
+    if (isLiked) deleteLikedImage(indexDb, image)
+    else addLikedImage(indexDb, image)
+
     setIsLiked(!isLiked)
   }
 
